27fa5b251767c95964791a0c08392f78e6cdc96b
hyobiin9 <moone1234@kakao.com>
2024-12-16 14:23:53 +0900

    feat: 2-8

--- main.ts
+++ main.ts
@@ -1,20 +1,10 @@
-let monsterName: string;
-let level: number;
-let hasGold: boolean;
-let skill: undefined;
-let area: null;
+const current: [number, number] = [0, 0];
+const target: [number, number] = [4, 5];
+const dx: number = target[0] - current[0];
+const dy: number = target[1] - current[1];
+console.log(`오른쪽으로 ${dx} 위쪽으로 ${dy} 만큼 이동!`);
 
-monsterName = "코드잇 고블린";
-level = 255;
-hasGold = false;
-skill = undefined;
-area = null;
-
-console.log(
-  `${monsterName}의 레벨은 ${level}이고,\n` +
-    `${
-      hasGold ? "해치우면 골드를 얻는" : "해치워도 골드를 주지 않는"
-    } 몬스터입니다.\n` +
-    `${skill ? `가진 능력은 ${skill} 이고 \n` : ""}` +
-    `${area ? `출현 지역은 ${area} 입니다.` : ""}`
-);
+const items: string[] = [];
+items.push("갑옷");
+items.push("빨간 물약");
+console.log(`${items.join(", ")}을/를 획득했다!`);
