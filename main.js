3eadae7fb4b4d4d5c00685165f7dc49237e22731
hyobiin9 <moone1234@kakao.com>
2024-12-16 20:38:26 +0900

    feat: 6-2

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 "use strict";
-const itemTable = [
-    ["name", "이름"],
-    ["price", "가격"],
-    ["description", "설명"],
-];
-const item = {
-    id: "h001",
-    name: "힐링 포션",
-    price: 200,
-    description: "마시면 체력을 50 회복한다.",
-};
-for (let [propertyKey, propertyName] of itemTable) {
-    console.log(`${propertyName} | ${item[propertyKey]}`);
-}
+const stock = new Map();
+stock.set("g001", 1);
+stock.set("g002", 2);
+console.log(stock.get("g001"));
+console.log(stock.get("g002"));
--- main.ts
+++ main.ts
@@ -1,23 +1,5 @@
-interface Item {
-  id: string;
-  name: string;
-  price: number;
-  description: string;
-}
-
-const itemTable: [keyof Item, string][] = [
-  ["name", "이름"],
-  ["price", "가격"],
-  ["description", "설명"],
-];
-
-const item: Item = {
-  id: "h001",
-  name: "힐링 포션",
-  price: 200,
-  description: "마시면 체력을 50 회복한다.",
-};
-
-for (let [propertyKey, propertyName] of itemTable) {
-  console.log(`${propertyName} | ${item[propertyKey]}`);
-}
+const stock = new Map<string, number>();
+stock.set("g001", 1);
+stock.set("g002", 2);
+console.log(stock.get("g001"));
+console.log(stock.get("g002"));
