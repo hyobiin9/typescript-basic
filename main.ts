24a12436cbbfe52150c1997ec4ec2ecc261867b7
hyobiin9 <moone1234@kakao.com>
2024-12-16 16:19:21 +0900

    feat: 4-2

--- main.ts
+++ main.ts
@@ -1,9 +1,33 @@
-enum Job {
-  Knight = "Knight",
-  Archer = "Archer",
-  Mage = "Mage",
-  Priest = "Priest",
-  Thief = "Thief",
+interface Entity {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-console.log(Job);
+interface Monster extends Entity {
+  name: string;
+  level: number;
+  hasGold?: boolean;
+  skills: string[];
+}
+
+let monster: Monster = {
+  id: "g001",
+  name: "고블린",
+  level: 22,
+  skills: ["태권도", "특공무술"],
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+console.log(
+  `${monster.name}(${monster.id})의 레벨은 ${monster.level}이고,\n` +
+    `${
+      monster.hasGold ? "해치우면 골드를 얻는" : "해치워도 골드를 주지 않는"
+    } 몬스터입니다.\n` +
+    `${
+      monster.skills.length > 0
+        ? `가진 능력은 ${monster.skills.join(", ")}입니다.`
+        : ""
+    }`
+);
